7233c126d8eae26b57714b00e27adbe440cdb3c7
Ala Eddine KHEFIFI <gio88_spider@hotmail.com>
2014-03-16 17:20:10 +0100

    Update index.rst

--- DependencyInjection/NzoFileDownloaderExtension.php
+++ DependencyInjection/NzoFileDownloaderExtension.php
@@ -8,10 +8,12 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * FileDownloader.
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * @author Ala Eddine Khefifi <alakhefifi@gmail.com>
+ * Website   www.alakhefifi.com
  */
+
 class NzoFileDownloaderExtension extends Extension
 {
     /**
