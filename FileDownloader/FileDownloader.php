744e310312e12f17bf6ca35500747eff3abc4ba5
nayzo <alakhefifi@gmail.com>
2015-04-18 18:51:03 +0200

    code amelioration

--- FileDownloader/FileDownloader.php
+++ FileDownloader/FileDownloader.php
@@ -32,25 +32,25 @@ class FileDownloader
     public function downloadFile($path, $newName = null)
     {
         $this->path = $this->path . $path;
-        if (!file_get_contents($this->path))
-            return false;
-
-        if (substr($this->path, -1) === '/' || substr($this->path, -1) === '#')
-            $this->path = substr($this->path, 0, -1);
-
-        if ($newName) {
-            if (preg_match('/[^"]+\.[a-z|0-9]+$/i', $newName))
-                $this->fileName = $newName;
-            else
-                return false;
-
-        } else {
-            $this->fileName = substr($this->path, strrpos($this->path, '/') + 1, strlen($this->path));
+        if (!file_get_contents($this->path)) {
+            return;
         }
 
+        if (substr($this->path, -1) === '/' || substr($this->path, -1) === '#') {
+            $this->path = substr($this->path, 0, -1);
+        }
+
+        if (null === $newName) {
+            $this->fileName = substr($this->path, strrpos($this->path, '/') + 1, strlen($this->path));
+        } else {
+            if (preg_match('/[^"]+\.[a-z|0-9]+$/i', $newName)) {
+                $this->fileName = $newName;
+            } else {
+                return;
+            }
+        }
 
         $response = new Response();
-
         $response->setStatusCode(200);
         $response->headers->set('Content-Type', mime_content_type($this->path));
         $response->headers->set('Content-Disposition', 'attachment;filename="' . $this->fileName . '"');
